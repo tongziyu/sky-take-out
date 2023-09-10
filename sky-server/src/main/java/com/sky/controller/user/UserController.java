8beff43e20113a5941974f2e218e00ff0985a261
tongziyu <65508655+tongziyu@users.noreply.github.com>
2023-09-10 21:26:59 +0800

    feat: 添加了微信小程序登录功能

---
+++ sky-server/src/main/java/com/sky/controller/user/UserController.java
@@ -0,0 +1,60 @@
+package com.sky.controller.user;
+
+import com.sky.constant.JwtClaimsConstant;
+import com.sky.dto.UserLoginDTO;
+import com.sky.entity.User;
+import com.sky.properties.JwtProperties;
+import com.sky.result.Result;
+import com.sky.service.UserService;
+import com.sky.utils.JwtUtil;
+import com.sky.vo.UserLoginVO;
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Description:
+ * @Author: Ian
+ * @Date: 2023/9/10 17:49
+ */
+@RestController
+@RequestMapping("/user/user")
+@Slf4j
+@Api(tags = "C端用户相关接口")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private JwtProperties jwtProperties;
+
+    @PostMapping("/login")
+    public Result<UserLoginVO> login(@RequestBody UserLoginDTO userLoginDTO){
+        log.info("C端登录唯一码:{}",userLoginDTO.getCode());
+
+        User user = userService.wxLogin(userLoginDTO);
+
+        Map claims = new HashMap();
+
+        claims.put(JwtClaimsConstant.USER_ID,user.getId());
+
+        String jwt = JwtUtil.createJWT(jwtProperties.getUserSecretKey(), jwtProperties.getUserTtl(), claims);
+
+        UserLoginVO token = UserLoginVO.builder()
+                .id(user.getId())
+                .openid(user.getOpenid())
+                .token(jwt)
+                .build();
+
+        return Result.success(token);
+
+
+    }
+}
---
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -0,0 +1,21 @@
+package com.sky.mapper;
+
+import com.sky.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+/**
+ * @Description:
+ * @Author: Ian
+ * @Date: 2023/9/10 20:41
+ */
+@Mapper
+public interface UserMapper {
+
+
+    @Select("select * from user where openid = #{openId}")
+    User selectByOpenId(String openId);
+
+    Integer insert(User user);
+
+}
