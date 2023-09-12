0b9596c0353c32a9a1396a157a0b3b399ae49528
tongziyu <65508655+tongziyu@users.noreply.github.com>
2023-09-12 12:58:54 +0800

    feat: 小程序端 添加购物车代码

--- sky-server/src/main/java/com/sky/controller/user/ShoppingCartController.java
+++ sky-server/src/main/java/com/sky/controller/user/ShoppingCartController.java
@@ -1,7 +1,14 @@
 package com.sky.controller.user;
 
+import com.sky.dto.ShoppingCartDTO;
+import com.sky.result.Result;
+import com.sky.service.ShoppingCartService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,4 +22,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Slf4j
 public class ShoppingCartController {
+
+    @Autowired
+    private ShoppingCartService shoppingCartService;
+
+
+    @PostMapping("/add")
+    @ApiOperation("添加购物车")
+    public Result addShoppingCart(@RequestBody ShoppingCartDTO shoppingCartDTO){
+
+        log.info("需要往购物车里面添加的数据:{}",shoppingCartDTO);
+        shoppingCartService.add(shoppingCartDTO);
+
+
+        return Result.success();
+    }
+
 }
---
+++ sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
@@ -0,0 +1,99 @@
+package com.sky.service.impl;
+
+import com.sky.context.BaseContext;
+import com.sky.dto.ShoppingCartDTO;
+import com.sky.entity.Dish;
+import com.sky.entity.Setmeal;
+import com.sky.entity.ShoppingCart;
+import com.sky.mapper.DishMapper;
+import com.sky.mapper.SetmealMapper;
+import com.sky.mapper.ShoppingCartMapper;
+import com.sky.service.ShoppingCartService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * @Description:
+ * @Author: Ian
+ * @Date: 2023/9/12 02:03
+ */
+@Service
+@Slf4j
+public class ShoppingCartServiceImpl implements ShoppingCartService {
+    @Autowired
+    private ShoppingCartMapper shoppingCartMapper;
+
+    @Autowired
+    private DishMapper dishMapper;
+
+    @Autowired
+    private SetmealMapper setmealMapper;
+
+
+    /**
+     * 添加购物车
+     * @param shoppingCartDTO
+     */
+    @Override
+    public void add( ShoppingCartDTO shoppingCartDTO) {
+        ShoppingCart shoppingCart = new ShoppingCart();
+
+        // 复制属性给shoppingCart对象
+        BeanUtils.copyProperties(shoppingCartDTO,shoppingCart);
+
+        // 从本地线程变量中获取userId;
+        Long userId = BaseContext.getCurrentId();
+
+        shoppingCart.setUserId(userId);
+
+        log.info("shoppingCart对象:{}",shoppingCart);
+
+        /*
+        逻辑: 首先去查询购物车中有没有这条数据,如果有就直接数量加+1
+                如果没有这条数据,则新建一条数据
+         */
+        List<ShoppingCart> list = shoppingCartMapper.list(shoppingCart);
+
+        // 判断 list 如果不是空的,则进行数量+1
+        if (list != null && list.size() >0){
+
+            ShoppingCart shoppingCart1 = list.get(0);
+
+            Integer number = shoppingCart1.getNumber();
+
+            shoppingCart1.setNumber(number + 1);
+
+            // 修改数据库表的数据
+            shoppingCartMapper.update(shoppingCart1);
+        }
+
+        // 如果list中没有数据,则表示需要直接添加一条数据
+        if (list == null || list.size() == 0){
+
+            // 判断是否是dish
+            if (shoppingCart.getDishId() != null){
+
+                Dish dish = dishMapper.selectById(shoppingCart.getDishId());
+                shoppingCart.setImage(dish.getImage());
+                shoppingCart.setAmount(dish.getPrice());
+                shoppingCart.setName(dish.getName());
+                log.info("查询出来的dish数据{}",dish);
+            }else{
+                Setmeal setmeal = setmealMapper.selectById(shoppingCart.getSetmealId());
+                shoppingCart.setImage(setmeal.getImage());
+                shoppingCart.setAmount(setmeal.getPrice());
+                shoppingCart.setName(setmeal.getName());
+            }
+            shoppingCart.setNumber(1);
+            shoppingCart.setCreateTime(LocalDateTime.now());
+            log.info("需要插入到shoppingcart表中的数据:{}",shoppingCart);
+            shoppingCartMapper.insert(shoppingCart);
+        }
+    }
+}
